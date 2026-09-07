d6d62e00ef896e6d51689842410167fbb5bdae4e
Kartik Bakshi <kabakshi@hotmail.ca>
2026-09-07 16:21:35 -0400

    find word length exercise

--- ruby_basics/10_basic_enumerables/exercises/basic_enumerable_exercises.rb
+++ ruby_basics/10_basic_enumerables/exercises/basic_enumerable_exercises.rb
@@ -29,12 +29,17 @@ def find_low_inventory(inventory_list)
   # use #select to iterate through each item of the inventory_list (a hash)
   # return a hash of items with values less than 4
   inventory_list.select{|inventory, value| value < 4}
-
-
 end
 
 def find_word_lengths(word_list)
   # use #reduce to iterate through each item of the word_list (an array)
   # return a hash with each word as the key and its length as the value
   # hint: look at the documentation and review the reduce examples in basic enumerable lesson
+  
+  word_list.reduce(Hash.new(0)) do |hash, word|
+   hash[word] = word.length 
+   hash
+   
+  end
+
 end
